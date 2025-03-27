2de9ed9e9cd162b82a18940276ddaa3c93b44b76
Muhammad Abdullah <m.abdullah.web.dev@gmail.com>
2025-03-27 04:03:07 -0700

    first commit

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,57 @@
+<div >
+    <section class="login">
+        <div class="inner">
+            <div class="row">
+                <div class="col-md-6 p-0">
+                    <div class="cover-image">
+                        <img src="{{ asset('assets/images/ghit-login-cover.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6 p-0">
+                    <div class="login-options pt-5 d-flex flex-column align-items-center gap-4">
+                        <h2  class="fs-5 fw-semibold text-center">Sign In / Sign Up</h2>
+                        <p class="text-left fs-6">Log in or sign up using your most active platform to ensure you receive our winner's email.</p>
+                        <div class="option-btns w-100 d-flex flex-column align-items-center">
+                            <button class="google-btn login-btn w-100">
+                                <img src="{{ asset('assets/svg/google.svg') }}" alt="">Continue with Google
+                            </button>
+                            <button class="X-btn login-btn w-100">
+                                <img src="{{ asset('assets/svg/x-white.svg') }}" alt="">Continue with X
+                            </button>
+                            <button class="discord-btn login-btn w-100">
+                                <img src="{{ asset('assets/svg/discord-white.svg') }}" alt="">Continue with Discord
+                            </button>
+                        </div>
+                        <p class="text-center px-3">By continuing, you accept our <a class="text-red" href="/terms-and-conditions">Terms of Condition</a>. For our Privacy Policy, <a class="text-red" href="/privacy-policy">click here</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="intro-section">
+        <div class="inner">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 col-xl-10 px-4">
+                        <h2 class="opacity-75 text-uppercase fs-6 fs-md-5 mb-2 mb-md-4">
+                            YOUR TICKETS, YOUR TRIBE
+                        </p>
+                        <p class="fw-bold fs-5 fs-md-3 mb-4 mb-md-5">
+                            Ghitloot is where you connect, celebrate, and win together. As an active member of your favorite creator’s
+                            or brand’s community, you collect tickets to join exciting giveaways. Dive into the fun and feel rewarded
+                            as a true part of the community.
+                        </p>
+                        <p class="fs-md-4">
+                            Our platform allows communities to create and manage their own giveaways, designed to celebrate success and
+                            reward loyalty to their followers and offer exciting giveaways without requiring any purchase.
+                        </p>
+                    </div>
+                </div>
+                <!-- Image -->
+                {{-- <img class="position-absolute top-0 start-50 translate-middle-x img-fluid w-50 w-md-25"
+                     src="{{ asset('assets/images/ghitloot-boxed-gray.png') }}"
+                     alt="Ghitloot Boxed Image"> --}}
+            </div>
+        </div>
+    </section>
+</div>
