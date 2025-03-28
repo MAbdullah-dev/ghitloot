fe348aef7f181ee5c6ea1e199612953a476fd446
Muhammad Abdullah <m.abdullah.web.dev@gmail.com>
2025-03-28 03:43:50 -0700

    login frontend mostly done

--- resources/views/components/layouts/loginBase.blade.php
+++ resources/views/components/layouts/loginBase.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png') }}">
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
